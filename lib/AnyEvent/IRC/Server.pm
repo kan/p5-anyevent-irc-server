4f44736904a62dae06a46e469680a3bd2b87ec94
Kan Fushihara <kan@mobilefactory.jp>
2010-01-22 12:47:34 +0900

    first commit

---
+++ lib/AnyEvent/IRC/Server.pm
@@ -0,0 +1,33 @@
+package AnyEvent::IRC::Server;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::IRC::Server -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::IRC::Server;
+
+=head1 DESCRIPTION
+
+AnyEvent::IRC::Server is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
