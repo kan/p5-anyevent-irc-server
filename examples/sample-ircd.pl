29e003a0a1480ec40bef7160c7bc52149ab23ec4
Kan Fushihara <kan.fushihara@gmail.com>
2012-10-21 23:13:54 +0900

    fix example

--- examples/sample-ircd.pl
+++ examples/sample-ircd.pl
@@ -14,7 +14,7 @@ GetOptions(
 );
 
 my $ircd = AnyEvent::IRC::Server->new(
-    host         => '192.168.186.128',
+    host         => '127.0.0.1',
     port         => $port,
     'servername' => 'localhost'
 );
