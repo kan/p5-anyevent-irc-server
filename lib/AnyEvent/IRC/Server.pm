051b4fe926d081a3f203753149d9bb9c21c61bf6
Kan Fushihara <kan.fushihara@gmail.com>
2013-05-02 15:58:51 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 29f0890..01dd6a0 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension AnyEvent::IRC::Server
    
     {{$NEXT}}
    
    +0.03 2013-05-02T06:58:43Z
    +    - fixed many bugs
    +
     0.02    Mon Oct 18 01:34:15 2010
             - support daemon_cmd_join
               (thanks yappo)

--- lib/AnyEvent/IRC/Server.pm
+++ lib/AnyEvent/IRC/Server.pm
@@ -2,7 +2,7 @@ package AnyEvent::IRC::Server;
 
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use base qw/Object::Event/;
 use AnyEvent::Handle;
 use AnyEvent::Socket;
